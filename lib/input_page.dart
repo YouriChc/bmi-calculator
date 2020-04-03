bb5d6859f707571b00d18b2f666cb3bffe26f248
Youri Chcouropat <youri.chcouropat.ext@orange.com>
2020-04-03 18:22:59 +0200

    Make the first pressable button + refactor the code

---
+++ lib/input_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottoomContainerHeight = 80.0;
+const bottomContainerCOlor = Color(0xFFFF5B1A);
+
+enum Gender { MALE, FEMALE }
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.MALE;
+                      });
+                    },
+                    color: selectedGender == Gender.MALE ? activeCardColor : inactiveCardColor,
+                    child: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      text: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.FEMALE;
+                      });
+                    },
+                    color: selectedGender == Gender.FEMALE ? activeCardColor : inactiveCardColor,
+                    child: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      text: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerCOlor,
+            margin: EdgeInsets.only(top: 10),
+            height: bottoomContainerHeight,
+            width: double.infinity,
+          )
+        ],
+      ),
+    );
+  }
+}
